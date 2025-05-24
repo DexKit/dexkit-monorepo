ed4f42b609ad7af8563cf251b79f7dc54b56fbb1
JoaoCampos89 <j.campos893@gmail.com>
2025-05-24 10:24:19 -0300

    fix: dexappadmin build

--- apps/dexappadmin/src/modules/admin/dashboard/edits/feat-usage-edit.tsx
+++ apps/dexappadmin/src/modules/admin/dashboard/edits/feat-usage-edit.tsx
@@ -1,12 +1,16 @@
-import { BooleanInput, Button, Edit, SimpleForm, TextInput, TopToolbar, useNotify, useRecordContext } from "react-admin";
+import {
+  BooleanInput,
+  Button,
+  Edit,
+  SimpleForm,
+  TextInput,
+  TopToolbar,
+  useNotify,
+  useRecordContext,
+} from "react-admin";
 import { useRemoveDomainMutation } from "../../hooks/payment";
 
-
-
-
-
 const FeatUsageActions = () => {
-
   const record = useRecordContext();
   const removeCustomDomainMutation = useRemoveDomainMutation();
   const notify = useNotify();
@@ -14,35 +18,37 @@ const FeatUsageActions = () => {
   function handleRemoveCustomDomain() {
     try {
       removeCustomDomainMutation.mutateAsync({ siteId: record?.siteId });
-      notify('Domain removed', { type: 'success' })
+      notify("Domain removed", { type: "success" });
     } catch {
-      notify('Failed to remove domain', { type: 'warning' })
+      notify("Failed to remove domain", { type: "warning" });
     }
   }
 
   return (
     <TopToolbar>
-      <Button color="error" variant="contained" onClick={handleRemoveCustomDomain}>Remove Custom Domain</Button>
+      <Button
+        color="error"
+        variant="contained"
+        onClick={handleRemoveCustomDomain}
+      >
+        <>Remove Custom Domain</>
+      </Button>
     </TopToolbar>
-  )
+  );
 };
 
-
-
 export const FeatUsageEdit = () => {
-
-  return (<>
-
-    <Edit actions={<FeatUsageActions />}>
-      <SimpleForm>
-        <TextInput source="account" disabled variant="outlined" />
-        <TextInput source="slug" disabled variant="outlined" />
-        <TextInput source="site.slug" disabled variant="outlined" />
-        <TextInput source="site.domain" disabled variant="outlined" />
-        <BooleanInput source="active"></BooleanInput>
-      </SimpleForm>
-    </Edit>
-
-  </>
-  )
+  return (
+    <>
+      <Edit actions={<FeatUsageActions />}>
+        <SimpleForm>
+          <TextInput source="account" disabled variant="outlined" />
+          <TextInput source="slug" disabled variant="outlined" />
+          <TextInput source="site.slug" disabled variant="outlined" />
+          <TextInput source="site.domain" disabled variant="outlined" />
+          <BooleanInput source="active"></BooleanInput>
+        </SimpleForm>
+      </Edit>
+    </>
+  );
 };
