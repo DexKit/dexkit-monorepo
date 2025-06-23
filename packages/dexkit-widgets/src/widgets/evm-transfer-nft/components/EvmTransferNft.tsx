3e9d1ffd2782d53423b50f798e13de266a3222f0
JoaoCampos89 <j.campos893@gmail.com>
2025-06-23 11:49:05 -0300

    fix: ens not resolving name

--- packages/dexkit-widgets/src/widgets/evm-transfer-nft/components/EvmTransferNft.tsx
+++ packages/dexkit-widgets/src/widgets/evm-transfer-nft/components/EvmTransferNft.tsx
@@ -80,7 +80,6 @@ export default function EvmTransferNft({
 
   const nftTransfer = useNftTransfer({
     contractAddress,
-    provider,
     onSubmit: (hash: string) => {
       if (hash && chainId) {
         createNotification({
