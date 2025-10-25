DexKit/dexkit-monorepo

commit a0a65d0625f647ab4aa6f7974d80ec0b611ba354

feat: remove duplicated code (#245)
* chore: remove duplicated code

packages/dexappbuilder-viewer/components/sections/AssetSection/index.tsx
@@ -1,0 +1,136 @@
+import { NETWORK_FROM_SLUG } from "@dexkit/core/constants/networks";
+import AssetLeftSection from "@dexkit/ui/modules/nft/components/AssetLeftSection";
+import AssetOptionsProvider from "@dexkit/ui/modules/nft/components/AssetOptionsProvider";
+import AssetRightSection from "@dexkit/ui/modules/nft/components/AssetRightSection";
+import { fetchAssetForQueryClient } from "@dexkit/ui/modules/nft/services/query";
+import DarkblockWrapper from "@dexkit/ui/modules/wizard/components/DarkblockWrapper";
+import { AssetPageSection } from "@dexkit/ui/modules/wizard/types/section";
+import { hexToString } from "@dexkit/ui/utils";
+import { useAsyncMemo } from "@dexkit/widgets/src/hooks";
+import { Alert, Box, Grid, NoSsr, Typography } from "@mui/material";
+import { useQueryClient } from "@tanstack/react-query";
+import { ThirdwebSDKProvider, useContract } from "@thirdweb-dev/react";
+import { useWeb3React } from "@web3-react/core";
+import { Suspense, useEffect } from "react";
+import { FormattedMessage } from "react-intl";
+import EditionDropSection from "../EditionDropSection";
+
+interface DropWrapperProps {
+  tokenId: string;
+  address: string;
+  network: string;
+}
+
+function DropWrapper({ tokenId, address, network }: DropWrapperProps) {
+  const { data: contract } = useContract(address);
+
+  const isDrop = useAsyncMemo(
+    async () => {
+      if (!contract) {
+        return false;
+      }
+      try {
+        const contractType = hexToString(await contract?.call("contractType"));
+
+        return contractType === "DropERC1155";
+      } catch (err) {
+        return false;
+      }
+    },
+    false,
+    [contract]
+  );
+
+  if (isDrop) {
+    return (
+      <EditionDropSection
+        section={{
+          type: "edition-drop-section",
+          config: {
+            network,
+            tokenId: tokenId as string,
+            address: address as string,
+          },
+        }}
+      />
+    );
+  }
+
+  return (
+    <Box>
+      <Alert severity="warning">
+        <Typography>
+          <FormattedMessage
+            id="drops.are.not.available.for.this.contract"
+            defaultMessage="Drops are not available for this contract"
+          />
+        </Typography>
+      </Alert>
+    </Box>
+  );
+}
+
+export interface AssetSectionProps {
+  section: AssetPageSection;
+}
+
+export default function AssetSection({ section }: AssetSectionProps) {
+  const { address, tokenId, network, enableDrops, enableDarkblock } =
+    section.config;
+
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const chainId = NETWORK_FROM_SLUG(network)?.chainId;
+
+    if (chainId) {
+      fetchAssetForQueryClient({
+        item: { chainId, contractAddress: address, tokenId },
+        queryClient,
+      });
+    }
+  }, [address, tokenId, network, queryClient]);
+
+  const { provider } = useWeb3React();
+
+  return (
+    <AssetOptionsProvider
+      key={`${network}-${address}-${tokenId}`}
+      options={{ options: {} }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <AssetLeftSection address={address} id={tokenId} />
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <AssetRightSection address={address} id={tokenId} />
+          {enableDarkblock && (
+            <NoSsr>
+              <Suspense>
+                <DarkblockWrapper
+                  address={address as string}
+                  tokenId={tokenId}
+                  network={network}
+                />
+              </Suspense>
+            </NoSsr>
+          )}
+        </Grid>
+        {enableDrops && (
+          <Grid item xs={12}>
+            <ThirdwebSDKProvider
+              activeChain={NETWORK_FROM_SLUG(network)?.chainId}
+              signer={provider?.getSigner()}
+            >
+              <DropWrapper
+                address={address}
+                tokenId={tokenId}
+                network={network}
+              />
+            </ThirdwebSDKProvider>
+          </Grid>
+        )}
+      </Grid>
+    </AssetOptionsProvider>
+  );
+}

packages/dexkit-ui/modules/nft/components/AssetImage.tsx
@@ -1,54 +1,95 @@
 import { ipfsUriToUrl } from "@dexkit/core/utils";
-import { Box } from "@mui/material";
+import { Avatar, Box } from "@mui/material";
 import Image from "next/future/image";
 import { useIntl } from "react-intl";
+import useLightbox from "../../../components/lightBox/useLightBox";
 import { isWhitelistedDomain } from "../../../utils/image";
 
 interface Props {
-  src: string;
+  src?: string;
+  enableLightBox?: boolean;
 }
 
-export function AssetImage({ src }: Props) {
+export function AssetImage({ src, enableLightBox }: Props) {
   const { formatMessage } = useIntl();
+  const { openLightbox, renderLightbox } = useLightbox();
 
   return (
-    <Box
-      sx={{
-        position: "relative",
-        width: "100%",
-        paddingTop: "100%",
-      }}
-    >
-      {isWhitelistedDomain(src) ? (
-        <Image
-          src={ipfsUriToUrl(src)}
-          style={{
-            position: "absolute",
-            top: 0,
+    <>
+      {src &&
+        renderLightbox({
+          slides: [
+            {
+              src: ipfsUriToUrl(src),
+              alt: "Image zoomed",
+            },
+          ],
+          render: { buttonNext: () => null, buttonPrev: () => null },
+        })}
+      {src && (
+        <Box
+          onClick={enableLightBox ? openLightbox : undefined}
+          sx={{
+            position: "relative",
             width: "100%",
+            paddingTop: "100%",
+            cursor: "pointer",
           }}
-          fill
-          alt={formatMessage({
-            id: "nft.image",
-            defaultMessage: "NFT Image",
-          })}
-        />
-      ) : (
-        <Image
-          src={ipfsUriToUrl(src)}
-          unoptimized={true}
-          style={{
-            position: "absolute",
-            top: 0,
-            width: "100%",
-          }}
-          fill
-          alt={formatMessage({
-            id: "nft.image",
-            defaultMessage: "NFT Image",
-          })}
-        />
+        >
+          {isWhitelistedDomain(src) ? (
+            <Image
+              src={ipfsUriToUrl(src)}
+              style={{
+                position: "absolute",
+                top: 0,
+                width: "100%",
+              }}
+              fill
+              alt={formatMessage({
+                id: "nft.image",
+                defaultMessage: "NFT Image",
+              })}
+            />
+          ) : (
+            <Image
+              src={ipfsUriToUrl(src)}
+              unoptimized={true}
+              style={{
+                position: "absolute",
+                top: 0,
+                width: "100%",
+              }}
+              fill
+              alt={formatMessage({
+                id: "nft.image",
+                defaultMessage: "NFT Image",
+              })}
+            />
+          )}
+        </Box>
       )}
-    </Box>
+      {!src && (
+        <Box
+          sx={{
+            position: "relative",
+            width: "100%",
+            paddingTop: "100%",
+          }}
+        >
+          <Avatar
+            alt={"No image"}
+            style={{
+              position: "absolute",
+              top: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            {" "}
+            No image available
+          </Avatar>
+        </Box>
+      )}
+    </>
   );
 }

packages/dexkit-ui/modules/nft/components/AssetLeftSection.tsx
@@ -1,0 +1,31 @@
+import { Grid, Skeleton } from "@mui/material";
+import { useAsset } from "../hooks";
+import { AssetDetails } from "./AssetDetails";
+import { AssetMedia } from "./AssetMedia";
+
+export function AssetLeftSection({
+  address,
+  id,
+}: {
+  address: string;
+  id: string;
+}) {
+  const { data: asset } = useAsset(address, id);
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        {asset ? (
+          <AssetMedia asset={asset} enableImageLightbox={true} />
+        ) : (
+          <Skeleton />
+        )}
+      </Grid>
+      <Grid item xs={12}>
+        <AssetDetails address={address} id={id} />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default AssetLeftSection;

packages/dexkit-ui/modules/nft/components/AssetMedia.tsx
@@ -9,9 +9,10 @@ import { AssetVideo } from "./AssetVideo";
 
 interface Props {
   asset: Asset;
+  enableImageLightbox?: boolean;
 }
 
-export function AssetMedia({ asset }: Props) {
+export function AssetMedia({ asset, enableImageLightbox }: Props) {
   const { data: metadata, isLoading } = useAssetMetadata(asset);
 
   if (isLoading) {
@@ -37,8 +38,11 @@ export function AssetMedia({ asset }: Props) {
             width: "100%",
           }}
         >
-          {nftSrcAndType.type === "image" && metadata?.image && (
-            <AssetImage src={metadata?.image} />
+          {nftSrcAndType.type === "image" && (
+            <AssetImage
+              src={metadata?.image}
+              enableLightBox={enableImageLightbox}
+            />
           )}
           {nftSrcAndType.type === "iframe" && nftSrcAndType.src && (
             <AssetIframe src={nftSrcAndType.src} />

packages/dexkit-ui/modules/nft/components/AssetPageActions.tsx
@@ -1,0 +1,154 @@
+import {
+  Divider,
+  Grid,
+  IconButton,
+  Paper,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { FormattedMessage } from "react-intl";
+
+import {
+  getBlockExplorerUrl,
+  getNetworkSlugFromChainId,
+  isAddressEqual,
+  truncateAddress,
+} from "@dexkit/core/utils/blockchain";
+import Link from "@dexkit/ui/components/AppLink";
+import LaunchIcon from "@mui/icons-material/Launch";
+import Share from "@mui/icons-material/Share";
+import { useWeb3React } from "@web3-react/core";
+import { useState } from "react";
+import Heart from "../../../components/icons/Heart";
+import {
+  useAsset,
+  useAssetBalance,
+  useAssetMetadata,
+  useFavoriteAssets,
+} from "../hooks";
+
+import { getWindowUrl } from "@dexkit/core/utils/browser";
+import ShareDialog from "./dialogs/ShareDialog";
+
+interface Props {
+  address: string;
+  id: string;
+}
+
+export function AssetPageActions({ address, id }: Props) {
+  const { data: asset } = useAsset(address, id);
+  const { data: metadata } = useAssetMetadata(asset);
+
+  const { account } = useWeb3React();
+  const { data: assetBalance } = useAssetBalance(asset, account);
+
+  const favorites = useFavoriteAssets();
+
+  const handleToggleFavorite = () => {
+    if (asset !== undefined) {
+      favorites.toggleFavorite({ ...asset, metadata });
+    }
+  };
+
+  const [openShare, setOpenShare] = useState(false);
+
+  const handleCloseShareDialog = () => setOpenShare(false);
+
+  const handleOpenShareDialog = () => setOpenShare(true);
+
+  return (
+    <>
+      <ShareDialog
+        dialogProps={{
+          open: openShare,
+          fullWidth: true,
+          maxWidth: "sm",
+          onClose: handleCloseShareDialog,
+        }}
+        url={`${getWindowUrl()}/asset/${getNetworkSlugFromChainId(
+          asset?.chainId
+        )}/${address}/${id}`}
+      />
+      <Grid container spacing={2} alignItems="stretch" alignContent="center">
+        <Grid item xs>
+          {asset?.protocol === "ERC721" ? (
+            <Paper variant="outlined" sx={{ p: 1, height: "100%" }}>
+              <Typography variant="caption" color="textSecondary">
+                <FormattedMessage id="owned.by" defaultMessage="Owned by" />
+              </Typography>
+              <Link
+                href={`${getBlockExplorerUrl(
+                  asset?.chainId
+                )}/address/${asset?.owner}`}
+                color="primary"
+                target="_blank"
+              >
+                <Stack
+                  component="span"
+                  direction="row"
+                  alignItems="center"
+                  alignContent="center"
+                  spacing={0.5}
+                >
+                  <div>
+                    {isAddressEqual(account, asset?.owner) ? (
+                      <FormattedMessage id="you" defaultMessage="you" />
+                    ) : (
+                      truncateAddress(asset?.owner)
+                    )}
+                  </div>
+                  <LaunchIcon fontSize="inherit" />
+                </Stack>
+              </Link>
+            </Paper>
+          ) : (
+            account &&
+            assetBalance?.balance && (
+              <Paper variant="outlined" sx={{ p: 2, height: "100%" }}>
+                <Typography>
+                  <FormattedMessage id="you.own" defaultMessage="You own" />:{" "}
+                  {assetBalance?.balance?.toString() || ""}{" "}
+                </Typography>
+              </Paper>
+            )
+          )}
+        </Grid>
+        <Grid item>
+          <Paper variant="outlined" sx={{ p: 1, height: "100%" }}>
+            <Stack
+              direction="row"
+              sx={{ height: "100%" }}
+              divider={<Divider flexItem orientation="vertical" />}
+              alignItems="center"
+              spacing={2}
+            >
+              <Tooltip
+                title={
+                  <FormattedMessage id="favorite" defaultMessage="Favorite" />
+                }
+              >
+                <IconButton onClick={handleToggleFavorite} disabled={!account}>
+                  <Heart
+                    sx={
+                      favorites.isFavorite(asset)
+                        ? (theme) => ({
+                            "& path": { fill: theme.palette.error.light },
+                          })
+                        : undefined
+                    }
+                  />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Share">
+                <IconButton onClick={handleOpenShareDialog}>
+                  <Share />
+                </IconButton>
+              </Tooltip>
+            </Stack>
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
+  );
+}

commit 900821be7edb6e1c59d8ca893161d4c4a6d9df7c

refactor: enhance asset metadata fetching and improve UI responsiveness
- Updated asset metadata fetching to handle ERC1155 protocol and token ID formatting (error was introduced in commit a0a65d06)
- adjusted layout and styling in AssetSection, AssetPricePaper, and AssetTabs for better responsiveness.
- Improved scrollbar styling in ListingsTable and OffersTable for enhanced user experience on responsive views

apps/dexappbuilder/pages/api/asset.ts
@@ -50,10 +50,15 @@ export default async function handler(
     console.log('failed fetching from api');
   }
 
-  const metadata = await getAssetMetadata(asset?.tokenURI, {
-    image: '',
-    name: `${asset.collectionName} #${asset.id}`,
-  });
+  const metadata = await getAssetMetadata(
+    asset?.tokenURI,
+    {
+      image: '',
+      name: `${asset.collectionName} #${asset.id}`,
+    },
+    asset?.protocol === "ERC1155",
+    asset?.id
+  );
 
   return res.json({ ...asset, metadata });
 }

packages/dexappbuilder-viewer/components/sections/AssetSection/index.tsx
@@ -108,9 +108,17 @@ export default function AssetSection({ section }: AssetSectionProps) {
       key={`${network}-${address}-${tokenId}`}
       options={{ options: {} }}
     >
-      <Box sx={{ px: { xs: 2, sm: 3, md: 4 } }}>
+      <Box sx={{
+        px: { xs: 3, sm: 3, md: 4 },
+        maxWidth: '100%',
+        overflow: 'hidden'
+      }}>
         <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
-          <Grid container spacing={2}>
+          <Grid container spacing={3}>
+            <Grid size={12} sx={{ mb: 2 }}>
+              <AssetLeftSection address={address} id={tokenId} />
+            </Grid>
+
             <Grid size={12}>
               <AssetPageTitle address={address} id={tokenId} />
             </Grid>
@@ -119,12 +127,9 @@ export default function AssetSection({ section }: AssetSectionProps) {
                 <AssetPageActions address={address} id={tokenId} />
               </NoSsr>
             </Grid>
-            <Grid size={12}>
-              <AssetPricePaper address={address} id={tokenId} />
-            </Grid>
 
             <Grid size={12}>
-              <AssetLeftSection address={address} id={tokenId} />
+              <AssetPricePaper address={address} id={tokenId} />
             </Grid>
 
             <Grid size={12}>
@@ -170,3 +175,4 @@ export default function AssetSection({ section }: AssetSectionProps) {
     </AssetOptionsProvider>
   );
 }
+

packages/dexkit-ui/modules/nft/components/AssetImage.tsx
@@ -33,7 +33,7 @@ export function AssetImage({ src, enableLightBox }: Props) {
           sx={{
             position: "relative",
             width: "100%",
-            paddingTop: "100%", // Aspect ratio 1:1
+            paddingTop: "100%",
             cursor: enableLightBox ? "zoom-in" : "default",
             transition: "all 0.2s ease-in-out",
             "&:hover": enableLightBox ? {

packages/dexkit-ui/modules/nft/components/AssetLeftSection.tsx
@@ -22,7 +22,7 @@ export function AssetLeftSection({
             sx={{
               position: "relative",
               overflow: "hidden",
-              paddingTop: { xs: "60%", sm: "100%" },
+              paddingTop: { xs: "75%", sm: "100%" },
             }}
           >
             <Skeleton
@@ -48,3 +48,4 @@ export function AssetLeftSection({
 }
 
 export default AssetLeftSection;
+

packages/dexkit-ui/modules/nft/components/AssetMedia.tsx
@@ -30,7 +30,7 @@ export function AssetMedia({ asset, enableImageLightbox }: Props) {
           sx={{
             position: "relative",
             overflow: "hidden",
-            paddingTop: { xs: "60%", sm: "100%" }, // Aspect ratio más pequeño en móviles
+            paddingTop: { xs: "75%", sm: "100%" },
           }}
         >
           <Skeleton

packages/dexkit-ui/modules/nft/components/AssetPageActions.tsx
@@ -130,41 +130,41 @@ export function AssetPageActions({ address, id }: Props) {
               alignItems="center"
               spacing={2}
             >
-               <Tooltip
-                 title={
-                   <FormattedMessage id="favorite" defaultMessage="Favorite" />
-                 }
-               >
-                 <span>
-                   <IconButton 
-                     onClick={handleToggleFavorite} 
-                     disabled={!account}
-                     sx={{ color: theme.palette.mode === 'dark' ? '#ffffff' : 'inherit' }}
-                   >
-                     <Heart
-                       sx={
-                         favorites.isFavorite(asset)
-                           ? (theme) => ({
-                             "& path": { fill: theme.palette.error.light },
-                           })
-                           : {
-                             "& path": {
-                               fill: theme.palette.mode === 'dark' ? '#ffffff' : 'inherit'
-                             }
-                           }
-                       }
-                     />
-                   </IconButton>
-                 </span>
-               </Tooltip>
-               <Tooltip title="Share">
-                 <IconButton 
-                   onClick={handleOpenShareDialog}
-                   sx={{ color: theme.palette.mode === 'dark' ? '#ffffff' : 'inherit' }}
-                 >
-                   <Share sx={{ color: theme.palette.mode === 'dark' ? '#ffffff' : 'inherit' }} />
-                 </IconButton>
-               </Tooltip>
+              <Tooltip
+                title={
+                  <FormattedMessage id="favorite" defaultMessage="Favorite" />
+                }
+              >
+                <span>
+                  <IconButton
+                    onClick={handleToggleFavorite}
+                    disabled={!account}
+                    sx={{ color: theme.palette.mode === 'dark' ? '#ffffff' : 'inherit' }}
+                  >
+                    <Heart
+                      sx={
+                        favorites.isFavorite(asset)
+                          ? (theme) => ({
+                            "& path": { fill: theme.palette.error.light },
+                          })
+                          : {
+                            "& path": {
+                              fill: theme.palette.mode === 'dark' ? '#ffffff' : 'inherit'
+                            }
+                          }
+                      }
+                    />
+                  </IconButton>
+                </span>
+              </Tooltip>
+              <Tooltip title="Share">
+                <IconButton
+                  onClick={handleOpenShareDialog}
+                  sx={{ color: theme.palette.mode === 'dark' ? '#ffffff' : 'inherit' }}
+                >
+                  <Share sx={{ color: theme.palette.mode === 'dark' ? '#ffffff' : 'inherit' }} />
+                </IconButton>
+              </Tooltip>
             </Stack>
           </Paper>
         </Grid>

packages/dexkit-ui/modules/nft/components/AssetPricePaper.tsx
@@ -365,8 +365,13 @@ export function AssetPricePaper({ address, id }: Props) {
         />
       )}
       <Grid size={12}>
-        <Paper variant="outlined" sx={{ p: 2 }}>
-          <Stack spacing={2} direction="row" alignItems="center">
+        <Paper variant="outlined" sx={{ p: { xs: 1.5, sm: 2 } }}>
+          <Stack
+            spacing={{ xs: 1, sm: 2 }}
+            direction={{ xs: "column", sm: "row" }}
+            alignItems={{ xs: "stretch", sm: "center" }}
+            sx={{ width: "100%" }}
+          >
             {/* <Box>
               <Typography variant="caption" color="textSecondary">
                 <FormattedMessage
@@ -385,6 +390,11 @@ export function AssetPricePaper({ address, id }: Props) {
                     onClick={handleOpenMakeListingDialog}
                     startIcon={<DollarSquare color="primary" />}
                     variant="outlined"
+                    sx={{
+                      minHeight: { xs: 48, sm: 40 },
+                      fontSize: { xs: '1rem', sm: '0.875rem' },
+                      py: { xs: 2, sm: 1.5 },
+                    }}
                   >
                     <FormattedMessage
                       defaultMessage="Sell"
@@ -403,6 +413,11 @@ export function AssetPricePaper({ address, id }: Props) {
                   onClick={handleOpenMakeOfferDialog}
                   startIcon={<Icon icon="tag" size="medium" color="primary" />}
                   variant="outlined"
+                  sx={{
+                    minHeight: { xs: 48, sm: 40 },
+                    fontSize: { xs: '1rem', sm: '0.875rem' },
+                    py: { xs: 2, sm: 1.5 },
+                  }}
                 >
                   <FormattedMessage
                     defaultMessage="Make Offer"

packages/dexkit-ui/modules/nft/components/AssetTabs.tsx
@@ -615,9 +615,12 @@ export function AssetTabs({ address, id }: Props) {
           <Tabs
             value={selectedTab}
             onChange={handleChangeTab}
+            variant="fullWidth"
             sx={{
               '& .MuiTab-root': {
                 color: 'text.primary',
+                fontSize: { xs: '0.875rem', sm: '1rem' },
+                minHeight: { xs: 48, sm: 40 },
                 '&.Mui-selected': {
                   color: 'primary.main',
                 },

packages/dexkit-ui/modules/nft/components/tables/ListingsTable.tsx
@@ -203,8 +203,21 @@ export function ListingsTable({
         </MenuItem>
       </Menu>
       <Stack spacing={1}>
-        <TableContainer>
-          <Table>
+        <TableContainer sx={{
+          overflowX: 'auto',
+          '&::-webkit-scrollbar': {
+            height: 8,
+          },
+          '&::-webkit-scrollbar-track': {
+            backgroundColor: 'rgba(0,0,0,0.1)',
+            borderRadius: 4,
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            borderRadius: 4,
+          },
+        }}>
+          <Table sx={{ minWidth: { xs: 600, sm: 'auto' } }}>
             <TableHead>
               <TableRow>
                 <TableCell component="th">

packages/dexkit-ui/modules/nft/components/tables/OffersTable.tsx
@@ -181,8 +181,21 @@ export function OffersTable({
         </MenuItem>
       </Menu>
       <Stack spacing={1}>
-        <TableContainer>
-          <Table>
+        <TableContainer sx={{
+          overflowX: 'auto',
+          '&::-webkit-scrollbar': {
+            height: 8,
+          },
+          '&::-webkit-scrollbar-track': {
+            backgroundColor: 'rgba(0,0,0,0.1)',
+            borderRadius: 4,
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            borderRadius: 4,
+          },
+        }}>
+          <Table sx={{ minWidth: { xs: 600, sm: 'auto' } }}>
             <TableHead>
               <TableRow>
                 <TableCell component="th">

packages/dexkit-ui/modules/nft/services/index.ts
@@ -48,7 +48,15 @@ export async function getAssetMetadata(
     uri = tokenURI.replace('0x{id}', tokenId);
   }
   if (isERC1155 && tokenId && tokenURI && tokenURI?.search('/{id}') !== -1) {
-    uri = tokenURI.replace('{id}', tokenId.length === 64 ? tokenId : Number(tokenId).toString(16).padStart(64, '0').toLowerCase());
+    let formattedTokenId = tokenId;
+
+    if (tokenId.length === 64) {
+      formattedTokenId = tokenId;
+    } else {
+      formattedTokenId = tokenId;
+    }
+
+    uri = tokenURI.replace('{id}', formattedTokenId);
   }
 
   if (tokenURI?.startsWith('data:application/json;base64')) {