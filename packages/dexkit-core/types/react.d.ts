8163d5ce80f5383001d1eb18f063e19c15703026
Eliécer Hernández <paraguanads@dexkit.com>
2025-11-02 12:53:34 -0300

    chore: dexappbuilder-render and dexkit-core are building

--- packages/dexkit-core/types/react.d.ts
+++ packages/dexkit-core/types/react.d.ts
@@ -2,7 +2,9 @@ declare module 'react' {
   export type Dispatch<A> = (value: A) => void;
   export type SetStateAction<S> = S | ((prevState: S) => S);
   export function createContext<T>(defaultValue: T): any;
+  export function useContext<T>(context: any): T;
   export function useState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
   export function useCallback<T extends (...args: any[]) => any>(callback: T, deps: any[]): T;
   export function useEffect(effect: () => void | (() => void), deps?: any[]): void;
+  export function useMemo<T>(factory: () => T, deps: any[]): T;
 }
